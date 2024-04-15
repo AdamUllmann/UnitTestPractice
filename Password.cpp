4a59cd095537b8b33cf2352f609752e82c7835d4
AdamUllmann <adamullmannbusiness@gmail.com>
2024-04-15 11:54:06 -0700

    added files

--- Password.cpp
+++ Password.cpp
@@ -4,17 +4,37 @@
 using std::string;
 
 /*
-  The function receives a string counts how many times the same character 
-  occurs at the beginning of the string, before any other characters (or the
-  end of the string). The function is case-sensitive so 'Z' is different than
-  'z' and any ASCII characters are allowed.
-*/
+   The function receives a string counts how many times the same character 
+   occurs at the beginning of the string, before any other characters (or the
+   end of the string). The function is case-sensitive so 'Z' is different than
+   'z' and any ASCII characters are allowed.
+   */
 int Password::count_leading_characters(string phrase){
-  int repetition = 1;
-  int index = 0;
-  while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
-    repetition++;
-    index++;
-  }
-  return repetition;
+	int repetition = 1;
+	int index = 0;
+	while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
+		repetition++;
+		index++;
+	}
+	return repetition;
 }
+
+bool has_mixed_case(string) {
+	if (isupper(string[0])) {
+		for (int i = 0; i < string.size(); i++) {
+			if (!isupper(string[i]) {
+				return 1;
+			}
+		}
+		return 0;
+	}
+	else {
+		for (int i = 0; i < string.size(); i++) {
+                        if (isupper(string[i]) {
+                                return 1;
+                        }
+                }
+                return 0;
+	}
+}
+
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,48 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+TEST(PasswordTest, single_letter_password_lowercase)
+{
+        Password my_password;
+        int actual = my_password.count_leading_characters("z");
+        ASSERT_EQ(1, actual);
+}
+TEST(PasswordTest, multiletter_password)
+{
+        Password my_password;
+        int actual = my_password.count_leading_characters("ZZ");
+        ASSERT_EQ(2, actual);
+}
+TEST(PasswordTest, mismatching_characters)
+{
+        Password my_password;
+        int actual = my_password.count_leading_characters("AB");
+        ASSERT_EQ(1, actual);
+}
+TEST(PasswordTest, large_password_size)
+{
+        Password my_password;
+        int actual = my_password.count_leading_characters("abcdefghijklmnopqrstuvwxyz");
+        ASSERT_EQ(1, actual);
+}
+TEST(PasswordTest, large_password_size_with_matching_leading_characters)
+{
+        Password my_password;
+        int actual = my_password.count_leading_characters("aaaaaaafghijklmnopqrstuvwxyz");
+        ASSERT_EQ(7, actual);
+}
+TEST(PasswordTest, differing_cases)
+{
+        Password my_password;
+        int actual = my_password.count_leading_characters("aAaA");
+        ASSERT_EQ(1, actual);
+}
+
+
+
+TEST(PasswordTest, differing_cases_for_new_test)
+{
+        Password my_password;
+        int actual = my_password.has_mixed_case("aAaA");
+        ASSERT_EQ(1, actual);
+}
